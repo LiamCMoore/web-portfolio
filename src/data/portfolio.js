a082100373ac70a329916bb651df3fd5f394f1a6
LiamCMoore <42259108+LiamCMoore@users.noreply.github.com>
2023-01-16 03:24:26 +1100

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Celeritas',
-        imgUrl: 'public/assets/CeleritasDemo.mp4',
+        imgUrl: '/assets/CeleritasDemo.mp4',
         stack: ['Unity', 'C#', 'Asset Forge', '3DS Max'],
         link: 'https://github.com/Liam-Harrison/Celeritas',
     },
     {
         title: 'Website Portfolio',
-        imgUrl: 'public/assets/Website Temp SS.jpg',
+        imgUrl: '/assets/Website Temp SS.jpg',
         stack: ['JS', 'React', 'Tailwind', 'Vite'],
         link: 'https://liamcmoore.github.io/web-portfolio/',
     },
     {
         title: 'Encrypter/Decrypter',
-        imgUrl: 'public/assets/Encrypter.jpg',
+        imgUrl: '/assets/Encrypter.jpg',
         stack: ['C++', 'Data Structures', 'Encryption'],
         link: 'https://github.com/LiamCMoore/encryptor-decryptor',
     },

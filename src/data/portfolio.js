8df2e2244e8eeedec93211808eeab7823037e34e
Liam Moore <LiamMoore96@gmail.com>
2023-01-15 23:58:18 +1100

    Initial Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        title: 'Celeritas',
+        imgUrl: '/assets/CeleritasDemo.mp4',
+        stack: ['Unity', 'C#', 'Asset Forge', '3DS Max'],
+        link: 'https://github.com/Liam-Harrison/Celeritas',
+    },
+    {
+        title: 'Website Portfolio',
+        imgUrl: '',
+        stack: ['JS', 'React', 'Tailwind', 'Vite'],
+        link: '',
+    },
+    {
+        title: 'Encrypter/Decrypter',
+        imgUrl: '',
+        stack: ['C++', 'React', 'Tailwind', 'Vite'],
+        link: '',
+    },
+
+]

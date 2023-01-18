02377b3883b252a27c0735940429edc6e7dd48bc
LiamCMoore <42259108+LiamCMoore@users.noreply.github.com>
2023-01-18 18:09:17 +1100

    Polish
    Added institutions to timeline. Added github icon to github portfolio links.

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,18 +4,21 @@ export default [
         imgUrl: '/web-portfolio/assets/CeleritasDemo.mp4',
         stack: ['Unity', 'C#', 'Asset Forge', '3DS Max'],
         link: 'https://github.com/Liam-Harrison/Celeritas',
+        git: 'True'
     },
     {
         title: 'Website Portfolio',
         imgUrl: '/web-portfolio/assets/Website Temp SS.jpg',
         stack: ['JS', 'React', 'Tailwind', 'Vite'],
         link: 'https://liamcmoore.github.io/web-portfolio/',
+        git: 'False'
     },
     {
         title: 'Encrypter/Decrypter',
         imgUrl: '/web-portfolio/assets/Encrypter.jpg',
         stack: ['C++', 'Data Structures', 'Encryption'],
         link: 'https://github.com/LiamCMoore/encryptor-decryptor',
+        git: 'True'
     },
 
 ]
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,31 +1,31 @@
 export default [
     {
     year: '2023',
-    title: 'On Call Local Area Tech',
+    title: 'On Call Local Area Tech - Orchard Grove Primary School',
     duration: '7 years',
     details: 'I perform technical support and maintence for Orchard Grove Primary School, as well as assisting with asset management and leasing.',
     },
     {
     year: '2022',
-    title: "Bachelor's Degree in Computer Science",
+    title: "Bachelor's Degree in Computer Science - Swinburne University",
     duration: '5 years',
     details: 'I finished my degree in computer science, learning about data structures, agile methodologies, several programming languages and artificial intelligence.',
     },
     {
     year: '2022',
-    title: "Bachelor's Degree in Games and Interactivity",
+    title: "Bachelor's Degree in Games and Interactivity - Swinburne University",
     duration: '5 years',
     details: 'I finished my degree in Games and Interactivity, learning about user focused testing practices, UX design, CI/CD development in large teams and project planning.',
     },
     {
     year: '2020',
-    title: 'Technical Specialist',
+    title: 'Technical Specialist - Navigo',
     duration: '2 years',
     details: 'I performed HR software training and updates for some of the largest companies in Australia, including 7 Eleven, Wesfarmers and Boral Pacific Mining. I also assisted in set-up of Microsoft Azure based software deployments as well as the maintaining and updating of Ascender Payroll Services.',
     },
     {
     year: '2015',
-    title: 'Diploma and Cert IV of Game Development',
+    title: 'Diploma and Cert IV of Game Development - Swinburne Tafe',
     duration: '2 years',
     details: 'I finished both a Cert IV and a Disploma in Games Development over two years, learning a variety of basic development principles and gaining experience with an abundance of development tools including 3DS Max, Photoshop, Unity and Illustrator.',
     },
